e11ed7680fa64d8d222898a85954003516a45a82
Dolores-dai <yuanli.dai@thoughtworks.com>
2020-01-20 11:28:16 +0800

    [Dai yuanli] add sass and echarts dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import ReactEcharts from 'echarts-for-react';
 
 const App: React.FC = () => {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <ReactEcharts
+                key={Math.random() + new Date().getTime()}
+                option={chartOptions}
+                onEvents={onEvents}
+                notMerge={true}
+                lazyUpdate={true}
+                style={{ height: '230px', left: '12px', top: '-8px' }}
+            />
         </div>
     );
+};
+
+const chartOptions = {
+    xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [
+        {
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            type: 'line'
+        }
+    ]
+};
+
+const charOnClick = () => {
+    alert('click!');
+};
+
+const onEvents = {
+    click: charOnClick
 };
 
 export default App;
